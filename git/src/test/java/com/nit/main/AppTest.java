4af1f5bb478bcf3e6f8730e05d9a9a9562383961
Shuja <Shuja@DESKTOP-667F718>
2024-11-08 15:58:55 +0530

    task1 by Developer

--- git/src/test/java/com/nit/main/AppTest.java
+++ git/src/test/java/com/nit/main/AppTest.java
@@ -1,19 +1,33 @@
 package com.nit.main;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
 public class AppTest {
-
-    /**
-     * Rigorous Test :-)
-     */
+	
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue(true);
+    public void sumWithPositiveNumber() {
+        
+    	int expected = 30;
+    	int x = 15;
+    	int y = 15;
+    	App ap = new App();
+    	int acctual = ap.sum(x, y);
+    	assertEquals(expected, acctual);
     }
+    
+    
+    @Test
+    public void sumWithNegativeNumber() {
+        
+    	int expected = -30;
+    	int x = -15;
+    	int y = -15;
+    	App ap = new App();
+    	int acctual = ap.sum(x, y);
+    	assertEquals(expected, acctual);
+    }
+    
 }
